a0e49a17a32ab4c73af364167242ec8932f53285
Chayne Thrash <chaynethrash@gmail.com>
2016-03-21 20:09:14 -0500

    Placing UI elements.

--- CookSmartMobileApp/www/js/index.js
+++ CookSmartMobileApp/www/js/index.js
@@ -15,12 +15,11 @@ var app = {
         //     window.location.href = "login.html";   
         //}
         document.addEventListener("backbutton", function() { navigator.app.exitApp(); }, false);
-        app.drawRefreshButton();
+        app.drawButtons();
         app.drawMenuButton();
         $(".list-header").on('click', function() { $(".device-list-container").toggle(); });
         
-        $("#refreshButton").on('click', app.updateDeviceList);
-        $("#deviceListItem").on('click', app.onDeviceSelection);
+        $("#refreshButton").on('click', app.updateDeviceStatus);
         $("#ConnectedToHub").hide();
         var hubInit = new HubInitializer( function(ip)  { app.onHubInitialization(ip); });
         hubInit.InitializeHub("");        
@@ -30,36 +29,47 @@ var app = {
         $('#ConnectingToHub').hide();
         $('#ConnectedToHub').show();
         this.serverIp = ip;
-        app.updateDeviceList();
+        app.updateDeviceStatus();
     },
     
-    updateDeviceList: function() {
+    updateDeviceStatus: function() {
         if (app.serverIp === null) {
             alert('Please wait until hub has been connected.');
         }
         else{
-            $.getJSON('http://' + app.serverIp + ':' + app.serverPort + '/GetCookerList', function(deviceIds) {
-                $("#DeviceList").empty();
-                $.each(deviceIds.docs, function(i, item) {
-                    $("#DeviceList").append('<a class="device-item" href="#" id="deviceListItem">' + item._id + '</a>');
-                });
-            });    
+            $.getJSON('http://' + app.serverIp + ':' + app.serverPort + '/GetDeviceStatus', function(response) {
+                if (response.status === "ok") {
+                    $("#device-status-text").text("Device status: " + response.deviceStatus);
+                    var buttonProperties = {
+                        text: (response.deviceStatus === 'idle') ? "Load Recipe" : "Stop",
+                        onClick: (response.deviceStatus === 'idle') ? app.openLoadRecipeModal : app.stopDevice
+                    };
+                    $("#device-button").text(buttonProperties.text);
+                    $("#device-button").on('click', buttonProperties.onClick);
+                } else {
+                    // handle error response.
+                }
+            }); // TODO: add a failure callback. 
         }
     },
     
-    onDeviceSelection: function() {
-        window.sessionStorage.deviceId = $(this).innerHtml;
-        window.href = 'app.html';
-    },
-    
-    drawRefreshButton: function() {
+    drawButtons: function() {
         var buttonDiameter = $(".refresh-button-container").width();
         $(".refresh-button-container").css({height: buttonDiameter + "px"});
+        $(".device-button-container").css({height: buttonDiameter + "px"});
     },
     
     drawMenuButton: function() {
         var buttonDiameter = $(".menu-selection-container").width();
-        $(".menu-selection-container").css({height: buttonDiameter + "px"});
+        $(".menu-selection-container").css({ height: buttonDiameter + "px" });
+    },
+    
+    openLoadRecipeModal() {
+        // do this
+    },
+    
+    stopDevice() {
+        // do this as well
     }
     
 };
